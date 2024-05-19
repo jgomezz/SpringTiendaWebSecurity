7d13260147dca488d243b23ca33427b30c3f5788
jgomezz <jgomezz@gmail.com>
2024-05-19 11:12:40 -0500

    udpate

--- src/main/java/pe/edu/tecsup/tienda/webs/ProfileController.java
+++ src/main/java/pe/edu/tecsup/tienda/webs/ProfileController.java
@@ -1,7 +1,6 @@
 package pe.edu.tecsup.tienda.webs;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.java.Log;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,16 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pe.edu.tecsup.tienda.entities.Usuario;
 
+@Log
 @Controller
 @RequestMapping("/profile")
 public class ProfileController {
 
-	private static final Logger logger = LoggerFactory.getLogger(ProfileController.class);
-	
 	@GetMapping
 	public String profile(@AuthenticationPrincipal Usuario usuario, Model model) {
-		logger.info("profile("+usuario+")");
-		
+		log.info("profile("+usuario+")");
 		return "profile/index";
 	}
 }
